6d88777a65ad6279306ecb6e24ceda40c07b24b7
Renan Luquete Cavalcante <renan.cavalcante@mercadolivre.com>
2021-05-03 21:09:35 -0300

    adding update favorites

--- src/main/kotlin/br/com/favorites/controller/FavoriteController.kt
+++ src/main/kotlin/br/com/favorites/controller/FavoriteController.kt
@@ -2,6 +2,8 @@ package br.com.favorites.controller
 
 import br.com.favorites.model.Favorite
 import br.com.favorites.repository.FavoriteRepository
+import org.slf4j.LoggerFactory
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.servlet.ModelAndView
 
@@ -11,17 +13,30 @@ class FavoriteController(
     private val favoriteRepository: FavoriteRepository
 ) {
 
+    private val logger = LoggerFactory.getLogger(FavoriteController::class.java)
+
     @PostMapping
     fun create(favorite: Favorite): ModelAndView {
+        logger.debug("creating, favorite:{}", favorite)
         favoriteRepository.save(favorite)
         val modelAndView = ModelAndView("index")
         modelAndView.addObject("listOfFavorites", getAllFavorites())
         return modelAndView
     }
 
+    @PostMapping("/update")
+    fun update(favorite: Favorite): ModelAndView {
+        logger.debug("updating, favorite:{}", favorite)
+        favoriteRepository.save(favorite)
+        val modelAndView = ModelAndView("redirect:/favorites")
+        modelAndView.addObject("listOfFavorites", getAllFavorites())
+        return modelAndView
+    }
+
     @PostMapping("/delete")
     fun delete(id: String): ModelAndView {
-        favoriteRepository.deleteById(id);
+        logger.debug("deleting, id:{}", id)
+        favoriteRepository.deleteById(id)
         val modelAndView = ModelAndView("redirect:/favorites")
         modelAndView.addObject("listOfFavorites", getAllFavorites())
         return modelAndView
@@ -30,6 +45,14 @@ class FavoriteController(
     @GetMapping("/create")
     fun formCreate(): ModelAndView {
         return ModelAndView("create")
+    }
+
+    @GetMapping("/update/{id}")
+    fun formUpdate(@PathVariable id: String): ModelAndView {
+        val favorite = favoriteRepository.findById(id).orElseThrow { Exception("Not found favorite by id:$id") }
+        val modelAndView = ModelAndView("update")
+        modelAndView.addObject("favorite", favorite)
+        return modelAndView
     }
 
     @GetMapping
